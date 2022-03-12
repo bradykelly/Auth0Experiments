6175604bb99df80b1abc44c2362746f8a1fe0625
bradykelly <brady@bradykelly.net>
2022-03-12 21:44:00 +0200

    Added Auth0.Mvc and SampleMvcApp

---
+++ Auth0.Mvc/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth0.Mvc.Controllers;
+
+public class AccountController : Controller
+{
+    [AllowAnonymous]
+    public async Task Login(string returnUrl = "/")
+    {
+        var authProps = new LoginAuthenticationPropertiesBuilder()
+            .WithRedirectUri(returnUrl)
+            .Build();
+        await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authProps);
+    }
+    
+    [Authorize]
+    public async Task Logout()
+    {
+        var authProps = new LogoutAuthenticationPropertiesBuilder()
+            .WithRedirectUri(Url.Action("Index", "Home")!)
+            .Build();
+        await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authProps);
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+}
---
+++ SampleMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
